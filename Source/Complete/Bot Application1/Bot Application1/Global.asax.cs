ac7b1da689ecc0164deaeb705a06a511294175cd
Laurent Ellerbach <laurelle@microsoft.com>
2018-03-20 14:45:28 +0100

    Updated nugets added inmemory storage for states

--- Source/Complete/Bot Application1/Bot Application1/Global.asax.cs
+++ Source/Complete/Bot Application1/Bot Application1/Global.asax.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +17,18 @@ namespace Bot_Application1
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            // Implement an in memory storage for the states
+            // Can be replaced by a SQL Storage or Cosmos DB.
+            // See https://docs.microsoft.com/en-us/bot-framework/dotnet/bot-builder-dotnet-state
+            Conversation.UpdateContainer(
+            builder =>
+            {
+                var store = new InMemoryDataStore();
+                builder.Register(c => store)
+                          .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                          .AsSelf()
+                          .SingleInstance();
+            });
         }
     }
 }
